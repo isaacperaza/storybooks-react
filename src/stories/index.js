7b1f8b454b9369070fdc19cecd4f679067598b36
Isaac Peraza Leon <isaac.peraza@gmail.com>
2019-08-12 12:47:16 -0700

    Config to dist folder

--- src/stories/index.js
+++ src/stories/index.js
@@ -5,12 +5,12 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 import { withKnobs, text } from '@storybook/addon-knobs/react';
 
-import { Welcome } from '@storybook/react/demo';
+// import { Welcome } from '@storybook/react/demo';
 import Button from './Button';
 
-storiesOf('Welcome', module).add('to Storybook', () => (
-  <Welcome showApp={linkTo('Button')} />
-));
+// storiesOf('Welcome', module).add('to Storybook', () => (
+//   <Welcome showApp={linkTo('Button')} />
+// ));
 
 storiesOf('Buttons', module)
   .addDecorator(withKnobs)
